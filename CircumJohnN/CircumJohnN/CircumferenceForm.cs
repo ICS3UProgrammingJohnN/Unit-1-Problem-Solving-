cc0d6b76bbe4159770369d8748926ae1386d30e1
John Ngundeng <36854092+JohnNgundeng@users.noreply.github.com>
2018-09-21 10:49:15 -0400

    Added CircumJohn project

---
+++ CircumJohnN/CircumJohnN/CircumferenceForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircumJohnN
+{
+    public partial class frmCircumference : Form
+    {
+        public frmCircumference()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // This closes the program when clicked
+            this.Close();
+        }
+    }
+}
